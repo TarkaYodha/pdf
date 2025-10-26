06a09b5315e9cbdbec56f0eaab08a4c1f954dae5
CREATOR <creator@CREATORs-MacBook-Air.local>
2025-10-26 23:01:54 +0530

    Fix routing for GitHub Pages: add base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/pdf-splitter-pro/',
   server: {
     host: "localhost",
     port: 5173,
