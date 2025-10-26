36903f5e346fc06574d4b84dbf23ba6f665f47b2
CREATOR <creator@CREATORs-MacBook-Air.local>
2025-10-26 23:40:27 +0530

    Fix: Update base path and project name

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/pdf-splitter-pro/',
+  base: '/',
   server: {
     host: "localhost",
     port: 5173,
